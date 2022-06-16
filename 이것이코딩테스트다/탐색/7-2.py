c260c060405eb062562e6d3f4730fba63ed96f80
ingkoon <ing97220@naver.com>
2022-06-16 16:12:32 +0900

    이것이코딩테스트다 정렬(완) 탐색(1)

---
+++ 이것이코딩테스트다/탐색/7-2.py
@@ -0,0 +1,25 @@
+#재귀함수로 구현한 이진 탐색 코드
+from array import array
+
+
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    # 찾은 경우 중간점 인덱스 반환
+    if array[mid] == target:
+        return mid
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid - 1)
+    else:
+        return binary_search(array,target, mid + 1, end)
+
+n, target = list(map(int, input().split()))
+array = list(map(int, input().split()))
+
+result = binary_search(array, target, 0, n-1)
+if result == None:
+    print(-1)
+else:
+    print(result + 1)
+
