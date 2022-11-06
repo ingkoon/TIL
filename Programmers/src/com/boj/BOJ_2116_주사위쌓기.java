430126a979542c598ae737c6361992d086072d85
ingkoon <ing97220@naver.com>
2022-11-06 23:25:34 +0900

    [2022-11-06] boj 8394 악수

---
+++ Programmers/src/com/boj/BOJ_2116_주사위쌓기.java
@@ -0,0 +1,11 @@
+package com.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_2116_주사위쌓기 {
+    public static void main(String[] args) {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
