4355779f0fa876a0400cebe0fe7763d2c98fba8c
ingkoon <ing97220@naver.com>
2022-12-12 10:00:35 +0900

    [2022-12-11] BOJ: 2251 물통 G5 (2)

--- Programmers/src/com/boj/BOJ_2251_물통.java
+++ Programmers/src/com/boj/BOJ_2251_물통.java
@@ -3,47 +3,67 @@ package com.boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Set;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 public class BOJ_2251_물통 {
-    static int A,B,C;
-    static boolean[] visited;
 
+    static int[] bucket;
+    static boolean[][] visited;
+
+    static Set<Integer> result;
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
-        A = Integer.parseInt(st.nextToken());
-        B = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
 
-        int[] bottles = new int[] {0, 0, C};
-        visited = new boolean[201];
+        bucket = new int[3];
+        for (int i = 0; i < bucket.length; i++) {
+            bucket[i] = Integer.parseInt(st.nextToken());
+        }
 
-        injection(bottles);
-        for (int i = 0; i < visited.length; i++) {
-            if(visited[i]) System.out.printf("%d ", i+1);
+        result = new TreeSet<>();
+        visited = new boolean[201][201];
+
+        dfs(0, 0, bucket[2]);
+        for (int r : result) {
+            System.out.printf(r + " ");
         }
     }
 
-    static void injection(int[] bottles){
-        if(visited[bottles[2]]) {
-            return;
-        }
-        System.out.println(bottles[2]);
-        visited[bottles[2]] = true;
-        int[] tmpA = bottles.clone();
-        int[] tmpB = bottles.clone();
-        int[] Atmp = bottles.clone();
-        int[] Btmp = bottles.clone();
-        // 다른 항아리에 주입하는 과정 수행
-        // 첫 번쨰 항아리에 물 담기
-        tmpA[0] += tmpA[2] - A - tmpA[0] > 0 ? tmpA[2] - A - tmpA[0] : tmpA[2];
-        tmpA[2] = Math.max(tmpA[2] - A - tmpA[0], 0);
-        injection(tmpA);
+    static void dfs(int a, int b, int c){
+        if(visited[a][b]) return;
 
-        // 두 번째 항아리에 물 담기
-        tmpB[1] += tmpB[2] - B - tmpB[1] > 0 ? tmpB[2] - B - tmpB[1] : tmpB[2];
-        tmpB[2] = Math.max(tmpB[2] - B - tmpB[1], 0);
-        injection(tmpB);
+        if(a == 0){
+            result.add(c);
+        }
+        visited[a][b] = true;
+        if(a + b > bucket[1]){ // a에서 b로 물 이동
+            dfs((a + b) - bucket[1], bucket[1], c);
+        } else{
+            dfs(0, a + b, c);
+        }
+
+        if(a + b > bucket[0]){ // b에서 a로 물 이동
+            dfs(bucket[0], (a + b) - bucket[0], c);
+        } else{
+            dfs(a + b, 0, c);
+        }
+
+        if(a + c > bucket[0]){ // c에서 a로 물 이동
+            dfs(bucket[0],b, a + c - bucket[0]);
+        } else{
+            dfs(a + c, b , 0);
+        }
+
+        if(b + c > bucket[1]) { // c에서 b로 물 이동
+            dfs(a, bucket[1],b + c  - bucket[1] );
+        }else{
+            dfs(a, b + c, 0);
+        }
+        // a에서 c로 물 이동
+        dfs(a, 0, b +c);
+        // b에서 c로 물 이동
+        dfs(0, b, a + c);
     }
 }
