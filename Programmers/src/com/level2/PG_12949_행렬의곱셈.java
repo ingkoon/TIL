6bf53e0e41bb7a6e409dd52ca0a70f095ad30f18
ingkoon <ing97220@naver.com>
2022-12-16 15:23:25 +0900

    [2022-12-16] PG: 12949 행렬의 곱셈

---
+++ Programmers/src/com/level2/PG_12949_행렬의곱셈.java
@@ -0,0 +1,30 @@
+package com.level2;
+
+import java.util.Arrays;
+
+public class PG_12949_행렬의곱셈 {
+    public static void main(String[] args) {
+        int[][] arr1 = new int[][] {{1,4}, {3,2}, {4,1}};
+        int[][] arr2 = new int[][] {{3,3}, {3,3}};
+
+        int[][] result = solution(arr1, arr2);
+
+        for (int[] ints : result) {
+            System.out.println(Arrays.toString(ints));
+        }
+    }
+
+    static int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] result = new int[arr1.length][arr2[0].length];
+
+        for(int i = 0 ; i < arr1.length ; ++i){
+            for(int j = 0 ; j < arr2[0].length ; ++j){
+                for(int k = 0 ; k < arr1[0].length ; ++k) {
+                    result[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+
+        return result;
+    }
+}
