02b529451a44c94574f72e7f002066a5d71a0f3f
ingkoon <ing97220@naver.com>
2023-01-02 22:16:22 +0900

    [2022-01-02] JPA 기본 실습(1)

---
+++ jpa기본/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,45 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        /*
+        2022.01.02 이인재
+        EntityManagerFactory
+        emf는 애플리케이션 로딩시점에서 단 하나만 생성하면된다.
+        * */
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        // 트랜잭션이 발생할 때마다 em을 만들어서 동작한다.
+        EntityManager em = emf.createEntityManager();
+
+        // 트랜잭션 생성 후 실행
+        EntityTransaction tx = em.getTransaction();
+        tx.begin(); // 이 코드는 좋은 코드가 아니다.
+
+        //code
+        try{
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("HelloB");
+
+            //영속 계층에 저장
+            em.persist(member);
+            tx.commit();
+        } catch (Exception e){
+            // 문제 발생시 rollback
+            tx.rollback();
+        } finally {
+            // em이 내부적으로 데이터베이스 커넥션을 가져가기 때문에 사용 후 종료해주어야 한다.
+            em.close();
+        }
+
+        //emf 종료
+        emf.close();
+
+    }
+}
