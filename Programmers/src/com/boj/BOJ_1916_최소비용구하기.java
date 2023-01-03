588750110487566c3fd636670aad4a8d39182a09
ingkoon <ing97220@naver.com>
2023-01-03 21:11:13 +0900

    [2022-01-03] BOJ: 1916 G4 최소비용구하기(2) - 주석 추가

--- Programmers/src/com/boj/BOJ_1916_최소비용구하기.java
+++ Programmers/src/com/boj/BOJ_1916_최소비용구하기.java
@@ -28,7 +28,7 @@ public class BOJ_1916_최소비용구하기 {
 
         for (int i = 0; i < n+1; i++)
             dist[i] = Integer.MAX_VALUE;
-        
+
         for (int i = 0; i < m; i++) {
             StringTokenizer st = new StringTokenizer(bf.readLine());
             int tmp = Integer.parseInt(st.nextToken()); // 출발지점
@@ -41,24 +41,24 @@ public class BOJ_1916_최소비용구하기 {
         start = Integer.parseInt(st.nextToken());
         end = Integer.parseInt(st.nextToken());
 
+        // 비용이 큰 순서대로 정렬한다.
         PriorityQueue<Node> pq = new PriorityQueue<Node>((o1, o2) -> Integer.compare(o1.cost, o2.cost));
-
-        pq.offer(new Node(start, 0));
-        dist[start] = 0;
+        pq.offer(new Node(start, 0)); // 시작 지점 삽입
+        dist[start] = 0; // 시작지점의 값을 초기화한다.
         while (!pq.isEmpty()){
             Node pNode = pq.poll();
-            if(pNode.idx == end){
+            if(pNode.idx == end){ // 도착지점의 idx와 일치한다는 것은 최단거리가 보장되었다는 뜻
                 System.out.println(dist[pNode.idx]);
                 break;
             }
-            if(dist[pNode.idx] < pNode.cost){
+            if(dist[pNode.idx] < pNode.cost){ // 현재 해당 노드까지의 비용이 현재노드의 비용보다 작을 경우는 패스
                 continue;
             }
-            for(int i = 0; i< graph[pNode.idx].size(); i++){
-                Node nNode = graph[pNode.idx].get(i);
-                if(dist[nNode.idx] > pNode.cost + nNode.cost){
-                    dist[nNode.idx] = pNode.cost + nNode.cost;
-                    pq.offer(new Node(nNode.idx, dist[nNode.idx]));
+            for(int i = 0; i< graph[pNode.idx].size(); i++){ // 해당 위치의 인덱스 개수만큼 반복
+                Node nNode = graph[pNode.idx].get(i); // 현재 노드를 가져온다.
+                if(dist[nNode.idx] > pNode.cost + nNode.cost){ //현재 노드까지의 값이 이전노드와 현재노드의 합보다 크다면
+                    dist[nNode.idx] = pNode.cost + nNode.cost; // 값을 교체 후
+                    pq.offer(new Node(nNode.idx, dist[nNode.idx])); // queue에 추가하여준다.
                 }
             }
         }
