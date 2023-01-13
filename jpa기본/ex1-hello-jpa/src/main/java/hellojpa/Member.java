ce447d7a8d6d8323859b5011b6307c7b156d14e7
ingkoon <dlswotmd@gmail.com>
2023-01-13 10:19:21 +0900

    [2023-01-13]엔티티 매핑

--- jpa기본/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ jpa기본/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
-//@Table(name = "USER")
+//@Table(name = "USER") 테이블 매핑하는 과정에서 사용한다. 기본적으로 엔티티 이름을 사용한다.
 public class Member {
     @Id
     private Long id;
