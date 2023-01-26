73a6ffa22c47c21d0e9a94a8ef22c27fd70a64ba
ingkoon <ing97220@naver.com>
2023-01-26 19:33:56 +0900

    [2023-01-26] TIL : Servlet에서 parameter를 받아 response를 날린다.

--- Spring/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ Spring/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -19,11 +22,15 @@ public class HellobootApplication {
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
 			servletContext.addServlet("hello", new HttpServlet() {
+
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					resp.setStatus(200);
-					resp.setHeader("Content-Type", "text/plain");
-					resp.getWriter().println("Hello Servlet");
+					String name = req.getParameter("name");
+
+
+					resp.setStatus(HttpStatus.OK.value()  );
+					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+					resp.getWriter().println("Hello " + name);
 				}
 			}).addMapping("/hello");
 		});
