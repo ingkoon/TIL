da3b0ebeea6b5c804b9ef8ac81c38ddc16291964
ingkoon <dlswotmd@gmail.com>
2023-03-17 08:32:21 +0900

    [2023-03-17] BOJ_1932_정수삼각형

---
+++ Algorithm/Programmers/src/com/level2/H_Index/PG_42747_HIndex.java
@@ -0,0 +1,14 @@
+package com.level2.H_Index;
+
+import java.util.Arrays;
+
+public class PG_42747_HIndex {
+    public static void main(String[] args) {
+        int[] citations = {3, 0, 6, 1, 5};
+        System.out.println(solution(citations));
+    }
+    static int solution(int[] citations){
+        Arrays.sort(citations);
+        return 0;
+    }
+}
