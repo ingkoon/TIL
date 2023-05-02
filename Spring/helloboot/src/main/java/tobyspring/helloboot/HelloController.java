f49895aa69191e9b3aba34081e99e27581dfacf1
ingkoon <dlswotmd@gmail.com>
2023-05-03 08:28:04 +0900

    [2023-05-03] Toby's Spring: HttpServletRequest에서 DispatcherServlet으로 변경

--- Spring/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ Spring/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,6 +1,7 @@
 package tobyspring.helloboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
@@ -17,8 +18,8 @@ public class HelloController {
     query String 타입으로 입력을 받아서 출력한다.
     */
     @GetMapping("/hello")
+    @ResponseBody
     public String hello(String name){
-
         return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
--- Spring/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ Spring/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -12,6 +12,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -23,40 +25,20 @@ import java.io.IOException;
 public class HellobootApplication {
 	public static void main(String[] args) {
 		// 빈 등록 정보, 리소스 접근 방법, 이벤트 전달-구독 방법 등을 수행해주는 Object = Spring container
-		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
 
 		// 빈 클래스 지정
 		applicationContext.registerBean(HelloController.class);
 		applicationContext.registerBean(SimpleHelloService.class);
+
 		// bean object 생성
 		applicationContext.refresh();
 
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("hello", new HttpServlet() {
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					// 인증, 보안 기능
-
-					if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())){
-						String name = req.getParameter("name");
-
-						// 이름 혹은 클래스 타입을 지정해 주면 된다.
-						HelloController helloController = applicationContext.getBean(HelloController.class);
-
-						String ret = helloController.hello(name);
-
-//						resp.setStatus(HttpStatus.OK.value()  );
-						//헤더 대신 사용하는 content type
-//						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-						resp.getWriter().println("Hello " + name);
-					}
-					else{
-						resp.setStatus(HttpStatus.NOT_FOUND.value());
-					}
-				}
-			}).addMapping("/hello");
+			servletContext.addServlet("dispatcherServlet",
+					new DispatcherServlet(applicationContext)
+			).addMapping("/*");
 		});
 
 		webServer.start();
