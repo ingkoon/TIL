7b2e8ad1b7083cd2c6fdbd518594c91fe7716846
ingkoon <dlswotmd@gmail.com>
2023-06-02 15:32:44 +0900

    [2023-06-02] meta annotation인 @Component Annotation을 통해 계층형 아키텍처를 구분한다.

--- Spring/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ Spring/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,11 +1,14 @@
 package tobyspring.helloboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
+//@RestController
+@RequestMapping("/")
 @RestController
 public class HelloController {
     private final HelloService helloService;
--- Spring/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ Spring/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -7,11 +7,16 @@ import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -22,26 +27,30 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
+@Configuration
+@ComponentScan
 public class HellobootApplication {
 	public static void main(String[] args) {
 		// 빈 등록 정보, 리소스 접근 방법, 이벤트 전달-구독 방법 등을 수행해주는 Object = Spring container
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext(){
+			@Override
+			protected void onRefresh(){
+				super.onRefresh();
 
-		// 빈 클래스 지정
-		applicationContext.registerBean(HelloController.class);
-		applicationContext.registerBean(SimpleHelloService.class);
-
-		// bean object 생성
+				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				WebServer webServer = serverFactory.getWebServer(servletContext -> {
+					servletContext.addServlet("dispatcherServlet",
+							new DispatcherServlet(this))
+							.addMapping("/*");
+				});
+				webServer.start();
+			}
+		};
+		applicationContext.register(HellobootApplication.class);
 		applicationContext.refresh();
 
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("dispatcherServlet",
-					new DispatcherServlet(applicationContext)
-			).addMapping("/*");
-		});
-
-		webServer.start();
+		// 빈 클래스 지정
 	}
 
 }
