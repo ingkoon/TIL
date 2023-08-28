6093c9aa8e11b15b016a6706fa790c8771f2fa1a
ingkoon <ing97220@naver.com>
2023-08-28 16:11:37 +0900

    [2023-08-28] BOJ 10830 거듭제곱

--- Algorithm/Programmers/src/com/boj/boj10830/Main.java
+++ Algorithm/Programmers/src/com/boj/boj10830/Main.java
@@ -3,52 +3,64 @@ package com.boj.boj10830;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
+//boj 10830 행렬 제곱
 public class Main {
-    static int a, b;
-    static int[][] proc;
+    static int n;
+    static long b;
+    static long[][] proc;
+    static long MOD = 1000L;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
 
-        a = Integer.parseInt(st.nextToken());
-        b = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        b = Long.parseLong(st.nextToken());
 
-        proc = new int [a][a];
-        int[][] temp = new int[a][a];
+        proc = new long[n][n];
 
-        for(int i = 0; i < a; i++){
+        for(int i = 0; i < n; i++){
             st = new StringTokenizer(bf.readLine());
-            for (int j = 0; j < a; j++) {
-                int num = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < n; j++) {
+                long num = Long.parseLong(st.nextToken()) % MOD;
                 proc[i][j] = num;
-                temp[i][j] = num;
             }
         }
 
-        for (int i = 0; i < b; i++) {
-            calc(temp);
-        }
+        long[][] result = calc(proc, b);
 
-        for (int[] ints : proc) {
-            System.out.println(Arrays.toString(ints));
+        for (long[] num : result) {
+            for (int i = 0; i < n; i++) {
+                System.out.print(num[i] + " ");
+            }
+            System.out.println();
         }
     }
 
-    static void calc(int[][] arr){
-        int[][] result = new int[a][a];
-        int tmp = 0;
-        int cnt = 0;
-        for (int i = 0; i < a; i++) {
-            int num = 0;
-            for (int j = 0; j < a; j++) {
-                num += proc[i][j] * arr[j][i];
-            }
-            result[i][cnt++%a] = num;
+    static long[][]  calc(long[][] arr, long exp){
+        if(exp == 1){
+            return arr;
         }
-        proc = result;
+        long[][] result = calc(arr, exp / 2);
+        result = multiply(result, result);
+        if(exp % 2 != 0){
+            result = multiply(result, proc);
+        }
+        return result;
+    }
+
+    static long[][] multiply(long[][] o1, long[][] o2){
+        long[][] result = new long[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    result[i][j] += o1[i][k] * o2[k][j];
+                    result[i][j] %= MOD;
+                }
+            }
+        }
+        return result;
     }
 }
