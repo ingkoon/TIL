5e5142d7fbd1092dfad6f520527dc864c06a779b
ingkoon <ing97220@naver.com>
2023-09-01 16:31:52 +0900

    [2023-09-01] BOJ 1018 S4 체스판 다시그리기

--- Algorithm/Programmers/src/com/boj/boj1018/Main.java
+++ Algorithm/Programmers/src/com/boj/boj1018/Main.java
@@ -1,9 +1,85 @@
 package com.boj.boj1018;
 
-public class Main {
-    static int n;
-    static int m;
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
+public class Main {
+    static int n, m, result = Integer.MAX_VALUE;
+    static int SIZE = 8;
+    static char[][] board;
+    static int[] dr = {0, 1, 0, -1};
+    static int[] dc = {1, 0, -1, 0};
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        board = new char[n][m];
+
+        for(int i = 0; i < n; i++){
+            char[] tmp = bf.readLine().toCharArray();
+            for (int j = 0; j < m; j++) {
+                board[i][j] = tmp[j];
+            }
+        }
+        for(int i = 0; i <= n - SIZE; i++){
+            for (int j = 0; j <= m - SIZE; j++) {
+               result = Math.min(result, Math.min(bfs(i, j, 'W'), bfs(i, j ,'B')));
+
+            }
+        }
+        System.out.println(result);
+    }
+    static int bfs(int r, int c, char color){
+        int tmp = 0;
+        char[][] tmpBoard = new char[SIZE][SIZE];
+        boolean[][] visited = new boolean[SIZE][SIZE];
+
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                tmpBoard[i][j] = board[r + i][c + j];
+            }
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(new Node(0, 0, color));
+        if(color != tmpBoard[0][0])
+            tmp++;
+        visited[0][0] = true;
+        while(!queue.isEmpty()){
+            Node node = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int nr = node.r + dr[i];
+                int nc = node.c + dc[i];
+                if(!isCheck(nr, nc) || visited[nr][nc])
+                    continue;
+                if(tmpBoard[nr][nc] == node.value){
+                    tmp++;
+                }
+                visited[nr][nc] = true;
+                queue.offer(new Node(nr, nc, node.value== 'W' ? 'B' : 'W'));
+            }
+        }
+
+        return tmp;
+    }
+
+    static boolean isCheck(int nr, int nc){
+        return 0<= nr && nr < SIZE && 0 <= nc && nc < SIZE;
+    }
+
+    static class Node{
+        int r;
+        int c;
+        char value;
+        public Node(int r, int c, char value) {
+            this.r = r;
+            this.c = c;
+            this.value = value;
+        }
     }
 }
