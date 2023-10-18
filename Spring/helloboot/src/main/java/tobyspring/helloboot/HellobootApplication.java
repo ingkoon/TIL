e3888f8c018ce68e6b2ce35f33bd4069e34273a6
ingkoon <ing97220@naver.com>
2023-10-18 15:50:25 +0900

    Add: 순수 서블릿 코드를 통한 웹서버 구동을 수행한다

--- Spring/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ Spring/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,48 +1,12 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.context.support.GenericWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-
-@Configuration
-@ComponentScan
 public class HellobootApplication {
-	@Bean
-	public ServletWebServerFactory servletWebServerFactory(){
-		return new TomcatServletWebServerFactory();
-	}
-	@Bean
-	public DispatcherServlet dispatcherServlet(){
-		return new DispatcherServlet();
-	}
-
 	public static void main(String[] args) {
-		// 빈 등록 정보, 리소스 접근 방법, 이벤트 전달-구독 방법 등을 수행해주는 Object = Spring container
-		SpringApplication.run(HellobootApplication.class, args);
+		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer();// 웹서버를 가져오는 함수
+		webServer.start();
 	}
 }
