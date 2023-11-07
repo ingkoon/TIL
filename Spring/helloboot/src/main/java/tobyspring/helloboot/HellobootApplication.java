b84d9231c469ec92a702071f5a32208bd45f6738
ingkoon <ing97220@naver.com>
2023-11-07 15:16:33 +0900

    Add: ServletContainer에 서블릿을 추가한다. 서블릿은 구현체인 HTTPServlet을 사용하였으며 override한 메서드의 service에 response를 설정한다.

--- Spring/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ Spring/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,10 +3,25 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class HellobootApplication {
 	public static void main(String[] args) {
 		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer();// 웹서버를 가져오는 함수
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					resp.setStatus(200);
+					resp.setHeader("Content-Type", "text/plain");
+					resp.getWriter().println("Hello Servlet");
+				}
+			}).addMapping("/hello");
+		});// 웹서버를 가져오는 함수
 		webServer.start();
 	}
 }
