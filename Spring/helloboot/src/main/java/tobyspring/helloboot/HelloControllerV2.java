2acc39a236478202a0e0dbbab46e628d48588a0e
ingkoon <ing97220@naver.com>
2023-11-08 16:45:20 +0900

    Add: Spring Container(ApplicationContext)에 HelloControllerV2를 등록하고 이를 통한 POJO를 수행한다.

--- Spring/helloboot/src/main/java/tobyspring/helloboot/HelloControllerV2.java
+++ Spring/helloboot/src/main/java/tobyspring/helloboot/HelloControllerV2.java
@@ -3,15 +3,19 @@ package tobyspring.helloboot;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 //@RestController
 @RequestMapping("/")
 @RestController
 public class HelloControllerV2{
-    /*
-    query String 타입으로 입력을 받아서 출력한다.
+    /**
+     * controller의 역할은 요청에 대한 검증을 수행하는 것이다.
+     * 따라서 Objects.requireNonNull()로 감싸서 반환을 수행한다
     */
     public String hello(String name){
-      return "Hello " + name;
+        SimpleHelloService helloService = new SimpleHelloService();
+        return  helloService.sayHello(Objects.requireNonNull(name));
     }
 
 }
