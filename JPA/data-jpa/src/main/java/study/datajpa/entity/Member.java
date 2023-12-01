94114eac3adc4afe7bc6e859488ea56404dbe678
ingkoon <ing97220@naver.com>
2023-12-01 16:39:43 +0900

    Learn: 엔티티 구현 및 테스트 수행

---
+++ JPA/data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,41 @@
+package study.datajpa.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "userName", "age"})
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String userName;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String userName) {
+        this.userName = userName;
+    }
+
+    public Member(String userName, int age, Team team) {
+        this.userName = userName;
+        this.age = age;
+        if(team != null){
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
