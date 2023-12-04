562db4883c58f663ad7bec2b238034fa62591ed7
ingkoon <dlswotmd@gmail.com>
2023-12-04 22:43:24 +0900

    [2023-12-04] BOJ 11444 G2 피보나치 수 6(1)

---
+++ Algorithm/Programmers/src/com/boj/boj11444/Main.java
@@ -0,0 +1,15 @@
+package com.boj.boj11444;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int n;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(bf.readLine());
+
+
+    }
+}
