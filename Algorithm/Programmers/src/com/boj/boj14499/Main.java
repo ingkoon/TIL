2ce103be318120ccb735e4b4bd24152452a00d7b
ingkoon <dlswotmd@gmail.com>
2024-03-20 00:01:40 +0900

    [2024-03-20] BOJ 14499 주사위 굴리기

--- Algorithm/Programmers/src/com/boj/boj14499/Main.java
+++ Algorithm/Programmers/src/com/boj/boj14499/Main.java
@@ -6,78 +6,123 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int r, c, x, y, k;
+    static int  x, y, k;
     static int[][] board;
     static int[] dr = {0 , 0, -1, 1};
     static int[] dc = {1, -1, 0, 0};
+
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
 
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
-        x = Integer.parseInt(st.nextToken());
         y = Integer.parseInt(st.nextToken());
+        x = Integer.parseInt(st.nextToken());
+
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
         k = Integer.parseInt(st.nextToken());
 
-        board = new int[r][c];
-        for (int i = 0; i < r; i++) {
-            st = new StringTokenizer(bf.readLine(), " ");
-            for (int j = 0; j < c; j++) {
+        board = new int[y][x];
+
+        for (int i = 0; i < y; i++) {
+            st = new StringTokenizer(bf.readLine());
+            for (int j = 0; j < x; j++) {
                 board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
+        Dice dice = new Dice(r, c);
+        st = new StringTokenizer(bf.readLine());
+
+        for (int i = 0; i < k; i++) {
+            int command = Integer.parseInt(st.nextToken()) - 1;
+
+            int nr = dice.r + dr[command];
+            int nc = dice.c + dc[command];
+
+            if(!isCheck(nr, nc)) continue;
+
+            dice.r = nr;
+            dice.c = nc;
+
+            dice.run(command);
+
+            if(board[nr][nc] == 0)
+                board[nr][nc] = dice.numbers[5];
+            else{
+                dice.numbers[5] = board[nr][nc];
+                board[nr][nc] = 0;
+            }
+
+            System.out.println(dice.numbers[0]);
+        }
     }
 
-    static boolean isCheck(int nr, int nc){
-        return 0 <= nr && nr < r && 0 <= nc && nc < c;
+    static boolean isCheck(int nr, int nc) {
+        return 0 <= nr && nr < y && 0 <= nc && nc < x;
     }
 
     static class Dice{
-        int up, down , left, right, front, back;
+        int r;
+        int c;
+        int[] numbers = new int[6];
 
-        public Dice(int up, int down, int left, int right, int front, int back) {
-            this.up = up;
-            this.down = down;
-            this.left = left;
-            this.right = right;
-            this.front = front;
-            this.back = back;
+        public Dice(int r, int c) {
+            this.r = r;
+            this.c = c;
         }
 
-        public void moveLeft(){
-            int tmp = left;
-            left = up;
-            up = right;
-            right = down;
-            down = tmp;
+        void run(int command){
+            switch (command){
+                case 0:
+                    right();
+                    break;
+                case 1:
+                    left();
+                    break;
+                case 2:
+                    up();
+                    break;
+                case 3:
+                    down();
+                    break;
+                default:
+                    break;
+            }
         }
 
-        public void moveRight(){
-            int tmp = right;
-            right = up;
-            up = left;
-            left = down;
-            down = tmp;
+        void left(){ // 왼쪽으로 이동했을 때
+            int tmp = numbers[5];
+            numbers[5] = numbers[2];
+            numbers[2] = numbers[0];
+            numbers[0] = numbers[3];
+            numbers[3] = tmp;
+
         }
 
-        public void moveUp(){
-            int tmp = front;
-            front = up;
-            up = back;
-            back = down;
-            down =tmp;
+        void right(){ // 오른쪽으로 이동했을 때
+            int tmp = numbers[5];
+            numbers[5] = numbers[3];
+            numbers[3] = numbers[0];
+            numbers[0] = numbers[2];
+            numbers[2] = tmp;
         }
 
-        public void moveDown(){
-            int tmp = back;
-            front = up;
-            up = back;
-            back = down;
-            down =tmp;
+        void up(){ // 위로 이동했을 때
+            int tmp = numbers[5];
+            numbers[5] = numbers[4];
+            numbers[4] = numbers[0];
+            numbers[0] = numbers[1];
+            numbers[1] = tmp;
         }
 
-
+        void down(){ // 아래로 이동했을 때
+            int tmp = numbers[5];
+            numbers[5] = numbers[1];
+            numbers[1] = numbers[0];
+            numbers[0] = numbers[4];
+            numbers[4] = tmp;
+        }
     }
 }
